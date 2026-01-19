fb2662e1777678629bb07feb925f055c8d59f147
VMMO23 <victormanuelmarinoq@gmail.com>
2026-01-19 17:54:55 -0500

    Organizaciones pequeñas al código

--- Inventario_TI/src/views/menu.jsx
+++ Inventario_TI/src/views/menu.jsx
@@ -5,9 +5,11 @@ export function Menu(){
     return(
 
         <article>
-
-            <h2>Menú</h2>
-            <p>Bienvenido Nombre_Empleado al menú de la plataforma de inventario</p>
+            <div>
+                <h2>Menú</h2>
+                <p>Bienvenido Nombre_Empleado al menú de la plataforma de inventario</p>
+            </div>
+            
             
             <R_Menu 
                 name="Inventario" 
@@ -16,8 +18,8 @@ export function Menu(){
             />
 
             <R_Menu 
-                name="Inventario" 
-                descripcion="Este permite registrar los productos que se envian o reciben para conocer con exactitud la cantidad de elementos con la que contamos dentro y fuera de la oficina." 
+                name="Seguimiento" 
+                descripcion="Este permite llevar un seguimiento constante de lo que necesita cada zona, ya sea evidenciado por el equipo de TI o por los líderes de cada zona." 
                 imagen="https://www.xamai.com/hubfs/Miniaturas%20junio%20(2)-3.webp"
             />
         </article>
