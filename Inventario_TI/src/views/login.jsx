b9cb14996bf483f2d3b362d97c62db7300e77703
ADSODaniM <168699938+ADSODaniM@users.noreply.github.com>
2026-01-21 12:56:34 -0500

    Carga de logo y Modificación estilo Login

--- Inventario_TI/src/views/login.jsx
+++ Inventario_TI/src/views/login.jsx
@@ -1,26 +1,57 @@
 import '../styles/Login.css';
 
-export function Login(){
-    return(
-        <section>
+import logo from '../assets/L.png';
 
+export function Login() {
+  return (
+    <section className="login-container">
+      <div className="login-wrapper">
+
+        {/* IZQUIERDA: logo y nombre */}
+        <div className="login-left">
+          <img src={logo} alt="Logo empresa" className="login-logo-side" />
+        </div>
+
+        {/* DERECHA: formulario */}
+        <div className="login-right">
+          <div className="login-card">
             <h2>Iniciar Sesión</h2>
 
-            <form action="">
-                <label htmlFor="usuario">Correo Electronico o Usuario:</label>
-                <input type="text" name="usuario" placeholder="Correo Electronico o Usuario"/>
+            <form>
+              <div className="form-group">
+                <label htmlFor="usuario">Correo electrónico o usuario</label>
+                <input
+                  type="text"
+                  id="usuario"
+                  name="usuario"
+                  placeholder="Ingresar usuario o correo"
+                />
+              </div>
 
-                <label htmlFor="contraseña">Contraseña:</label>
-                <input type="password" name="contraseña" placeholder='Contraseña' />
+              <div className="form-group">
+                <label htmlFor="contraseña">Contraseña</label>
+                <input
+                  type="password"
+                  id="contraseña"
+                  name="contraseña"
+                  placeholder="Ingresar contraseña"
+                />
+              </div>
 
-                <button action="send"> Iniciar Sesión</button>
-
+              <button on-click="" type="submit" className="login-btn">
+                Iniciar Sesión
+              </button>
             </form>
 
-            
-            <a href='https://www.google.com/'>¿Has olvidado tu contraseña?</a>
-        </section>
-    )
+            <a href="#" className="forgot-password">
+              ¿Has olvidado tu contraseña?
+            </a>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
 }
 
 export default Login;
